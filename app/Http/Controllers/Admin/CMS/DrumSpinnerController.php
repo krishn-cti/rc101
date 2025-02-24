b2eb813903b67e163c17e9fd03ec9cc02c2a4ed5
krish-cti <krishshukla.github@gmail.com>
2025-02-24 19:25:30 +0530

    old code but not in use

---
+++ app/Http/Controllers/Admin/CMS/DrumSpinnerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\CMS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DrumSpinnerController extends Controller
+{
+    //
+}
---
+++ app/Models/WeaponsSystem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WeaponsSystem extends Model
+{
+    use HasFactory;
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cms_weapons_systems';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'status',
+        'created_at'
+    ];
+
+    public function getImageAttribute($value)
+    {
+        if ($value) {
+            return asset('cms_images/' . $value);
+        } else {
+            return asset('admin/img/shop-img/no_image.png');
+        }
+    }
+}
